5ddb210944420b2795406998ce92afd09236059a
a Joshua Tree <133618057+Ayohinliveaid@users.noreply.github.com>
2024-10-20 15:22:24 +0800

    base configuration

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/Vue3Project/' //This base configuration ensures that your assets will be linked correctly when deployed to a subdirectory (/Vue3Project/) on GitHub Pages.
 })
