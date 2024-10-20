778fe9c24dd176ddbe857e4c507c08b4f83704f2
a Joshua Tree <133618057+Ayohinliveaid@users.noreply.github.com>
2024-10-20 15:52:23 +0800

    local server configuration

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/Vue3Project/' //This base configuration ensures that your assets will be linked correctly when deployed to a subdirectory (/Vue3Project/) on GitHub Pages.
+  base: process.env.NODE_ENV === 'production' ? '/Vue3Project/' : '/'
+  //This base configuration ensures that your assets will be linked correctly when deployed to a subdirectory (/Vue3Project/) on GitHub Pages. 
+  //While local development sever will not be affected
 })
