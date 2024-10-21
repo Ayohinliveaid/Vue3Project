3a79320287096ba1b41af391fee695b0969b38b7
a Joshua Tree <133618057+Ayohinliveaid@users.noreply.github.com>
2024-10-21 21:58:35 +0800

    intro page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,11 +10,12 @@
                             <el-button type="primary" class="buttonStyle" v-on:click="requestMainCats">
                                 more cats</el-button>
                             <el-space direction="vertical" alignment="normal">
-                                <div v-for="(cat, index) in mainCats" v-bind:key="index" 
-                                     class="cardDiv">
+                                <div v-for="(cat, index) in mainCats" v-bind:key="index" class="cardDiv">
                                     <el-card class="cardStyle">
                                         <img v-if="cat" v-bind:src="cat.url" alt="Cat Image" style="height: 300px;" />
                                     </el-card>
+
+
                                     <el-card style="flex:1" class="cardStyle">
                                         <el-button type="primary" class="buttonStyle"
                                             v-on:click="addToFavourites(cat)">favourite</el-button>
@@ -26,12 +27,11 @@
                         </el-tab-pane>
                         <el-tab-pane label="Animated Cats">
 
-                            <el-button type="primary" class="buttonStyle"
-                                v-on:click="requestAnimatedCats">more cats</el-button>
+                            <el-button type="primary" class="buttonStyle" v-on:click="requestAnimatedCats">more
+                                cats</el-button>
 
-                            <el-space direction="vertical" alignment="normal" >
-                                <div v-for="(cat, index) in animatedCats" v-bind:key="index"
-                                    class="cardDiv">
+                            <el-space direction="vertical" alignment="normal">
+                                <div v-for="(cat, index) in animatedCats" v-bind:key="index" class="cardDiv">
                                     <el-card class="cardStyle">
                                         <img v-if="cat" v-bind:src="cat.url" alt="Cat Image" style="height: 300px;" />
 
@@ -39,7 +39,7 @@
                                     <el-card style="flex:1" class="cardStyle">
                                         <el-button type="primary" class="buttonStyle"
                                             v-on:click="addToFavourites(cat)">favourite</el-button>
-                                            CatID: {{ cat.id }}
+                                        CatID: {{ cat.id }}
                                     </el-card>
                                 </div>
                             </el-space>
@@ -59,15 +59,16 @@
                                 <el-form label-width="100px" style="width:90%;margin-top: 25px;"
                                     v-bind:model="loginForm" v-bind:rules="loginRules" ref="loginForm">
                                     <el-form-item label="E-mail" prop="email">
-                                        <el-input v-model="loginForm.email"  clearable></el-input>
+                                        <el-input v-model="loginForm.email" clearable></el-input>
 
                                     </el-form-item>
                                     <el-form-item label="Password" prop="password">
-                                        <el-input v-model="loginForm.password"  type="password" show-password></el-input>
+                                        <el-input v-model="loginForm.password" type="password" show-password></el-input>
 
                                     </el-form-item>
                                     <el-form-item>
-                                        <el-button type="success" class="buttonStyle" v-on:click="login">Login</el-button>
+                                        <el-button type="success" class="buttonStyle"
+                                            v-on:click="login">Login</el-button>
 
                                     </el-form-item>
                                 </el-form>
@@ -209,7 +210,7 @@ export default {
         addToFavourites: function (cat) {
             if (this.isLoggedIn == false) {
                 alert("You need to login to save your favourites");
-                this.activeName="favourites";
+                this.activeName = "favourites";
 
 
             } else {
@@ -230,14 +231,14 @@ export default {
             }).catch((error) => {
                 // If validation fails
                 alert('Login failed: ' + error.message); // Optional error handling
-                this.activeName="favourites"
+                this.activeName = "favourites"
             });
         },
         logout: function () {
             this.isLoggedIn = false;
-            this.loginForm.email="";
-            this.loginForm.password="";
-            this.activeName="cats"
+            this.loginForm.email = "";
+            this.loginForm.password = "";
+            this.activeName = "cats"
         },
         validateEmail: function (rule, value, callback) {
             if (!value) {
@@ -249,7 +250,7 @@ export default {
         validatePassword: function (rule, value, callback) {
             if (!value) {
                 callback(new Error("enter valid password"));
-                this.activeName="favourite";
+                this.activeName = "favourite";
             } else {
                 callback();
             }
@@ -284,7 +285,7 @@ export default {
     top: 35%;
     left: 50%;
     transform: translate(-50%, -50%);
-    
+
 }
 
 .el-button.buttonStyle {
@@ -292,23 +293,23 @@ export default {
     float: right;
     border: none;
     color: rgba(82, 56, 23, 0.691);
-    margin-bottom: 10px;  
+    margin-bottom: 10px;
 }
 
 .el-button.buttonStyle:hover {
-    background-color: rgba(188, 133, 61, 0.691); /* Active background color */
+    background-color: rgba(188, 133, 61, 0.691);
+    /* Active background color */
     color: rgba(0, 0, 0, 0.691);
 }
 
-.cardDiv{
-    display: flex; width:750px; 
+.cardDiv {
+    display: flex;
+    width: 750px;
 }
 
-.el-card.cardStyle{
+.el-card.cardStyle {
     background-color: rgba(220, 196, 166, 0.691);
-    border:none;
+    border: none;
     margin-left: 10px;
 }
-
-
 </style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,46 +1,91 @@
-<template lang="">
+<template>
     <header class=header>
         <!-- <img src=""  alt="LOGO"/> -->
-          Cat Gallery
-            <ul>
-                <li><a href="#sec0">sec0</a></li>
-                <li><a href="#sec1">sec1</a></li>
-                <li><a href="#sec2">sec2</a></li>
-            </ul>
-        <router-link to="/" class="routerLink">Go Home</router-link>     
+        Cat Gallery
+        <ul>
+            <li><a href="#sec0">Introduction</a></li>
+            <li><a href="#sec1">Functions</a></li>
+            <li><a href="#sec2">Advantages</a></li>
+        </ul>
+        <router-link to="/" class="routerLink">Start Now</router-link>
     </header>
     <div></div>
-    <div id="sec0" class="sec0">sec0</div>
-    <div id="sec1" class="sec1">sec1
-        <table class="table" cellspacing="5">
+    <div id="sec0" class="sec0">
+        <img src="/public/CatFace.png" alt="CatFace" />
+        <div class="sec0div">
+            <h1>Welcome to the Electric Cat Gallery</h1>
+            <p>Explore a vibrant collection of stunning cat images powered by our advanced image sourcing technology.
+            </p>
+            <ul>
+                <li>Explore a vibrant collection of stunning cat images powered by our advanced image sourcing
+                    technology.</li>
+                <li>Instant previews for your favorite cat selections</li>
+                <li>Enjoy fast delivery on all image requests.</li>
+            </ul>
+            <router-link to="/" class="routerLink">Request Your Cat Image Now</router-link>
+        </div>
+    </div>
+    <div id="sec1" class="sec1">sec1: continuing updating!
+        <!-- <table class="table" cellspacing="5">
             <thead>
                 <tr>
-                    <th colspan=4><div>Advantages</div></th>
+                    <th colspan=4>
+                        <div>Advantages</div>
+                    </th>
                 </tr>
                 <tr>
-                    <th><div>h1</div></th>
-                    <th><div>h2</div></th>
-                    <th><div>h3</div></th>
-                    <th><div>h4</div></th>
+                    <th>
+                        <div>h1</div>
+                    </th>
+                    <th>
+                        <div>h2</div>
+                    </th>
+                    <th>
+                        <div>h3</div>
+                    </th>
+                    <th>
+                        <div>h4</div>
+                    </th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
-                    <td><div>d1</div></td>
-                    <td><div>d2</div></td>
-                    <td><div>d3</div></td>
-                    <td><div>d4</div></td>
+                    <td>
+                        <div>d1</div>
+                    </td>
+                    <td>
+                        <div>d2</div>
+                    </td>
+                    <td>
+                        <div>d3</div>
+                    </td>
+                    <td>
+                        <div>d4</div>
+                    </td>
                 </tr>
             </tbody>
-        </table>
+        </table> -->
 
     </div>
-    <div id="sec2" class="sec2">sec2</div>
+    <div id="sec2" class="sec2">
+        sec2: continuing updating!
+        <!-- <div class="scroll-container">
+            <button id="scroll-left" class="scroll-btn">Left</button>
+            <div class="scroll-section0">
+                <div class="scroll-item">Item 1</div>
+                <div class="scroll-item">Item 2</div>
+                <div class="scroll-item">Item 3</div>
+                <div class="scroll-item">Item 4</div>
+            </div>
+            <button id="scroll-right" class="scroll-btn">Right</button>
+        </div> -->
+    </div>
 </template>
 
 <script>
 export default {
-    name: "Login"
+    name: "Login",
+
 
 
 }
@@ -52,7 +97,7 @@ export default {
     display: flex;
     align-items: center;
     position: fixed;
-    width:100vw;
+    width: 100vw;
 
 }
 
@@ -97,42 +142,120 @@ export default {
     color: black;
     /* positon:absolute; */
     margin-left: auto;
-    margin-right:50px;
-       
+    margin-right: 50px;
+
+
 }
-.routerLink:hover{
+
+.routerLink:hover {
     background-color: rgb(213, 173, 120);
 }
 
-.sec0{
-    height:500px;
+.sec0 {
+    height: 500px;
     padding-top: 70px;
+    display: flex;
+}
+
+.sec0 img {
+    flex: 1;
+}
+
+.sec0div {
+    flex: 1;
+    height: 500px;
+    display: flex;
+    /* Add this to make it a flex container */
+    flex-direction: column;
+    /* Arrange children in a column */
+    /* justify-content: space-between;  */
+}
+
+h1 {
+    padding: 20px;
+}
+
+p {
+    padding: 20px;
+}
+
+.sec0 .routerLink {
+    height: 50px;
+    width: 240px;
+    background-color: rgb(232, 209, 179);
+    border: none;
+    border-radius: 4px;
+    padding-left: 30px;
+    padding-right: 30px;
+    margin-left: 20px;
+    margin-top: auto;
+
 
 }
-.sec1{
-    height:500px;
+
+
+
+.sec1 {
+    height: 500px;
     padding-top: 70px;
     display: flex;
     justify-content: center;
 
 }
-.sec2{
-    height:500px;
+
+.sec2 {
+    height: 500px;
     padding-top: 70px;
-    
+
 }
 
-.table{
+.table {
     width: 80vw;
     background-color: antiquewhite;
 }
-.table th{
+
+.table th {
     background-color: aqua;
     border-radius: 20px;
-    width:20vw;
+    width: 20vw;
 }
-.table td{
+
+.table td {
     background-color: aquamarine;
     border-radius: 20px;
+}
+
+
+
+.scroll-container {
+    display: flex;
+    align-items: center;
+}
+
+.scroll-section0 {
+    display: flex;
+    overflow-x: auto;
+    white-space: nowrap;
+    scroll-behavior: smooth;
+    /* Smooth scrolling effect */
+    width: 300px;
+    /* Width of the scrollable area */
+    margin: 0 10px;
+    /* Space between the buttons */
+}
+
+.scroll-item {
+    min-width: 100px;
+    margin-right: 10px;
+    background-color: lightblue;
+    text-align: center;
+    padding: 20px;
+}
+
+.scroll-btn {
+    background-color: lightgray;
+    border: none;
+    padding: 10px;
+    cursor: pointer;
 }
 </style>
